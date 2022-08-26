7cbe46f0aa290fff37c5b7b1c2ca4643f2d28ef8
paloma.souza <paloma.souza@ambevtech.com.br>
2022-08-26 12:56:23 -0300

    mark as completed

--- 02-js/todo-list/script.js
+++ 02-js/todo-list/script.js
@@ -1,22 +1,43 @@
-const button = document.getElementById('add')
-const input = document.getElementById('input')
-const todos = document.getElementById('todoContainer')
-const todoList = document.createElement('ul')
-todos.append(todoList)
+const button = document.getElementById('add');
+const input = document.getElementById('input');
+const todosContainer = document.getElementById('todosContainer');
 
-let text=''
-
-input.addEventListener('keyup',function(e) {
-    text = e.target.value
-})
+let todoList = [];
+let textId = '';
+input.addEventListener('keyup', function (e) {
+    button.disabled = false;
+    textId = e.target.value;
+    console.log(todoList);
+});
 
 button.addEventListener('click', function (e) {
-    e.preventDefault()
-    const todoItem = document.createElement('li')
-    todoList.appendChild(todoItem)
+    e.preventDefault();
+    const existTodo = todoList.filter(item => item.text === textId);
+    if (existTodo.length > 0) {
+        button.setAttribute('disabled', true);
+        return;
+    }
+    const todo = { text: textId, done: false };
+    todoList.push(todo);
 
-    todoItem.textContent = text
+    const todoItem = document.createElement('li');
+    todoItem.id = todoList.at(-1).text;
+    todoItem.textContent = todoList.at(-1).text;
+    todoItem.onclick = function () {
+        const clicked = todoList.find(todo => todo.text === this.id);
 
-    input.value = ''
-    input.focus()
-})
+        clicked.done ? this.classList.remove('done') : this.classList.add('done');
+        const newList = todoList.map(todo => {
+            if (todo.text === this.id) {
+                return { ...todo, done: !todo.done };
+            }
+            return todo;
+        });
+        todoList = [...newList];
+    };
+
+    todosContainer.appendChild(todoItem);
+
+    input.value = '';
+    input.focus();
+});
