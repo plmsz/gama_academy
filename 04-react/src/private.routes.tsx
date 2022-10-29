cb9fdfdb73a14a48f5e86ae49025693347857d9f
Paloma Souza <paloma.souza@ambevtech.com.br>
2022-10-29 18:46:38 -0300

    add private route

---
+++ 04-react/src/private.routes.tsx
@@ -0,0 +1,28 @@
+import { Redirect, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+
+const PrivateRoute = ({ component: Component, path: Path, ...rest }: any) => {
+  const isLoggedIn: string | null = localStorage.getItem('@gamaServiceToken');
+
+  const isSectionActive: any = () => {
+    if (isLoggedIn === null) {
+      return false;
+    } else {
+      const onlyToken = isLoggedIn;
+      const tokenPayload: any = jwt_decode(onlyToken);
+      const expSeconds = tokenPayload.exp;
+      const timeNow = Date.now() / 1000;
+      return timeNow > expSeconds ? false : true;
+    }
+  };
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isSectionActive() ? <Component {...props} /> : <Redirect to='/' />
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
--- 04-react/src/routes.tsx
+++ 04-react/src/routes.tsx
@@ -5,6 +5,7 @@ import Home from './pages/Home/Home';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import Dashboard from './pages/Dashboard';
+import PrivateRoute from './private.routes';
 
 function Routes() {
   return (
@@ -14,7 +15,7 @@ function Routes() {
         <Route path='/contact' component={Contact} />
         <Route path='/signin' component={SignIn} />
         <Route path='/signup' component={SignUp} />
-        <Route path='/dash' component={Dashboard} />
+        <PrivateRoute path='/dash' component={Dashboard} exact />
       </Switch>
     </BrowserRouter>
   );
